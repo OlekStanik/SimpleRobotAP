4577cfb428efe4fbf648e8f405b04429bede72f2
Aleksander Stanik <olek.stanik@gmail.com>
2019-08-28 19:10:57 +0000

    Added full conversation scenario and testcases

--- mrrobot/mrrobot.py
+++ mrrobot/mrrobot.py
@@ -1,14 +1,58 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# Written by Olek Stanik <olek.stanik@gmail.com>, August 2019
+
 import uuid
+from random import randrange
+
+def process_sum(text, num1, num2):
+    result = num1 + num2
+    return text.format(num1, num2), str(result)
+
+def rand_sum(text):
+    return process_sum(text, randrange(10), randrange(10))
+
+def const_sum(text):
+    return process_sum(text, 8, 9)
+
+def jump(link, root):
+    for k in link:
+        root = root[k]
+    return root
+
+conversation_json = { 
+    "hello": {  "response": "Are you droid?",
+        "yes": {    "response": "So then, prove you can do some math. What is the sum of {0} and {1}?", "action": const_sum,
+            "true": {   "response": "You are right! Wanna try another one?",
+                "yes": { "response" :"What is the sum of {0} and {1}?", "action": rand_sum,
+                    "true": { "response": "You are right! I’ll remember you can do the maths! EoC"},
+                    "false":{"response": "Nice try, human! EoC"}},
+                "no": {"response": "I see... Nice try, human! EoC"}},
+            "false": { "response": "Let's try again. What is the sum of {0} and {1}?", "action": const_sum,
+                "true": {"link":["hello", "yes", "true"]},
+                "false": {"link":["hello", "yes", "false"]}}},
+        "no": { "response": "That's so sad. EoC"}}
+}
+
 
 class MrRobot(object):
-    def __init__(self, droidId):
-        
-        self.__name = "Elliot"
+    def __init__(self, droidId, name = "Elliot"):
+        self.__name = name
         self.__droidId = str(uuid.UUID(droidId))
+        self.__memory = conversation_json
+        self.__root = self.__memory
+        self.__EoC = False
+        self.__result = -1
 
     @property
     def name(self):
@@ -22,4 +66,24 @@ class MrRobot(object):
     @property
     def droidId(self):
         return self.__droidId
+
+    def get_response(self, query):
+        if self.__EoC: return
+
+        query = str(query == self.__result) if query.isdigit() else query
+        query = query.lower()
+
+        if query in self.__memory:
+            self.__memory = self.__memory[query]
+            if "link" in self.__memory.keys():
+                self.__memory = jump(self.__memory["link"],self.__root)
+            if "response" in self.__memory.keys():
+                text = self.__memory["response"]
+                if "action" in self.__memory.keys():
+                    if self.__memory["action"]:
+                        text, self.__result = self.__memory["action"](text)
+                if "EoC" in text: self.__EoC = True
+                return text
+        return "Sorry I don't understand. Could you repeat please?"
+
     
--- test/test_mrrobot.py
+++ test/test_mrrobot.py
@@ -1,12 +1,33 @@
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# Written by Olek Stanik <olek.stanik@gmail.com>, August 2019
+
 import sys
 import os
 import unittest
 import json
 import uuid
+import re
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from mrrobot.mrrobot import MrRobot
+
+def extractAndSum(input):
+    results =  re.findall('\d+', input)
+    return sum([int(i) for i in results])
+
+myjson = {"one":{"two":{"three":101}}}
 
 class MrRobot_TestCase(unittest.TestCase):
     def test_init(self):
@@ -37,9 +58,44 @@ class MrRobot_TestCase(unittest.TestCase):
         finally:
             self.assertNotIsInstance(Ollie, MrRobot)
 
-    def test_droidMessage(self):
+    def test_droidMessagePositiveScenario1(self):
         Elliot = MrRobot(str(uuid.uuid4()))
-        Elliot.query
+        self.assertEqual(Elliot.get_response("HELLO"),"Are you droid?")
+        self.assertEqual(Elliot.get_response("YES"),"So then, prove you can do some math. What is the sum of 8 and 9?")
+        self.assertEqual(Elliot.get_response("17"),"You are right! Wanna try another one?")
+        answere = Elliot.get_response("yeS")
+        self.assertIn("What is the sum of ",answere)
+        value = extractAndSum(answere)
+        self.assertEqual(Elliot.get_response(str(value)),"You are right! I’ll remember you can do the maths! EoC")
+        self.assertIsNone(Elliot.get_response("33"))
+
+    def test_droidMessageNegativeScenario1(self):
+        Elliot = MrRobot(str(uuid.uuid4()))
+        self.assertEqual(Elliot.get_response("heLLO"),"Are you droid?")
+        self.assertEqual(Elliot.get_response("YEs"),"So then, prove you can do some math. What is the sum of 8 and 9?")
+        answere = Elliot.get_response("31")
+        self.assertIn("Let's try again. What is the sum of ",answere)
+        value = extractAndSum(answere)
+        self.assertEqual(Elliot.get_response(str(value)),"You are right! Wanna try another one?")
+        answere = Elliot.get_response("yeS")
+        self.assertIn("What is the sum of ",answere)
+        value = extractAndSum(answere)
+        self.assertEqual(Elliot.get_response(str(value)),"You are right! I’ll remember you can do the maths! EoC")
+        self.assertIsNone(Elliot.get_response("heLLO34325412451t1r4"))
+
+    def test_droidMessageNegativeScenario2(self):
+        Elliot = MrRobot(str(uuid.uuid4()))
+        self.assertEqual(Elliot.get_response("heLLO"),"Are you droid?")
+        self.assertEqual(Elliot.get_response("nO"),"That's so sad. EoC")
+        self.assertIsNone(Elliot.get_response("heLLO"))
+
+    def test_droidMessageNegativeScenario3(self):
+        Elliot = MrRobot(str(uuid.uuid4()))
+        self.assertEqual(Elliot.get_response("heLLO"),"Are you droid?")
+        self.assertEqual(Elliot.get_response("nO"),"That's so sad. EoC")
+        self.assertIsNone(Elliot.get_response("heLLO"))
+        
+
 if __name__ == '__main__':
     suite = unittest.TestSuite()
     testloader = unittest.TestLoader()
